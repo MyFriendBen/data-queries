43d904abeea925e209ccddfce540390f052fe416
SonaliBedge <sonali@codethedream.org>
2026-01-13 11:03:32 -0800

    Created mart_income dbt models with test, that reproduce the output of the data_income.sql materialized view.

---
+++ dbt/models/postgres/intermediate/int_income.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized='view',
+    description='Intermediate model for income data with enriched screener information - joins with data view for consistency'
+) }}
+
+SELECT
+    si.id,
+    si.screen_id,
+    si.type,
+    si.amount,
+    si.frequency,
+    d.id as screener_id,
+    d.submission_date,
+    d.white_label_id
+FROM {{ source('django_apps', 'screener_incomestream') }} si
+INNER JOIN public.data d ON si.screen_id = d.id
