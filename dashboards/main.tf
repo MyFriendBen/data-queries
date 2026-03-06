425b3b55def4bc98fc52f3afbec2330a2a008018
Josh Mejia <josh@myfriendben.org>
2026-03-06 16:42:25 -0700

    Switch Terraform backend from GCS to Terraform Cloud
    - Replace backend "gcs" with cloud block (org: MyFriendBen)
    - Add cli_config_credentials_token to both workflow files
    - Resolves GCS auth issue caused by GCP org policy blocking SA keys

--- dashboards/main.tf
+++ dashboards/main.tf
@@ -1,9 +1,11 @@
 terraform {
   required_version = ">= 1.0.0, < 2.0.0"
 
-  backend "gcs" {
-    bucket = "mfb-terraform-state"
-    prefix = "dashboards"
+  cloud {
+    organization = "MyFriendBen"
+    workspaces {
+      tags = ["dashboards"]
+    }
   }
 
   required_providers {
