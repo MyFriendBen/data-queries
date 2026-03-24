e4531997baa46c00fe32958976a929a0427a9091
Josh Mejia <josh@myfriendben.org>
2026-03-23 19:05:41 -0600

    Clarify data_tenant.sql GRANTs are per-tenant examples

--- data-queries/data_tenant.sql
+++ data-queries/data_tenant.sql
@@ -8,6 +8,10 @@ WHERE
     white_label_id
     = (regexp_match(current_user, '^wl_[a-z_]+_([0-9]+)_ro$'))[1]::int;
 
--- Restore permissions (adjust role names as needed based on your environment)
+-- Grant permissions to each tenant role (repeat for every wl_<state>_<id>_ro credential)
+-- Example for NC (white_label_id=5):
 GRANT USAGE ON SCHEMA public TO wl_nc_5_ro;
 GRANT SELECT ON public.data_tenant TO wl_nc_5_ro;
+-- Example for CO (white_label_id=1):
+-- GRANT USAGE ON SCHEMA public TO wl_co_1_ro;
+-- GRANT SELECT ON public.data_tenant TO wl_co_1_ro;
