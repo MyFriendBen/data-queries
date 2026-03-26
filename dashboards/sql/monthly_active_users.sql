b1be959e892a00a9b21a429c561668b5f106f772
Josh Mejia <josh@myfriendben.org>
2026-03-26 15:48:09 -0600

    Support multi-code tenants (e.g. CESN) in GA analytics queries
    Widen state_code regex to match slugs like co_energy_calculator (not just
    2-letter codes) and switch MAU query from single = to IN clause so tenants
    with multiple URL paths are counted together.

--- dashboards/sql/monthly_active_users.sql
+++ dashboards/sql/monthly_active_users.sql
@@ -2,6 +2,6 @@ SELECT
     DATE_TRUNC(event_date_parsed, MONTH) AS month,
     COUNT(DISTINCT user_pseudo_id) AS active_users
 FROM `analytics_internal`.`int_ga4_page_views`
-WHERE state_code = '${state_code}'
+WHERE state_code IN (${state_codes})
 GROUP BY month
 ORDER BY month
