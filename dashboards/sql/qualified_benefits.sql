d4ddad9dded4301110d2701336d49067ea1045f7
nihalerdal <nihalerdall@gmail.com>
2026-04-29 18:05:21 -0700

    Fix what percentage of completed screeners qualified for benefits filter by white label

--- dashboards/sql/qualified_benefits.sql
+++ dashboards/sql/qualified_benefits.sql
@@ -1,15 +1,29 @@
 WITH totals AS (
-    SELECT count(*) as total_count FROM analytics.mart_screener_data WHERE 1=1 [[AND {{submission_date}}]] [[AND {{partner}}]] [[AND {{county}}]]
+    SELECT count(*) AS total_count
+    FROM analytics.mart_screener_data
+    WHERE 1 = 1 [[AND {{submission_date}}]] [[AND {{partner}}]] [[AND {{county}}]]
 ),
+
 filter_keys AS (
-    SELECT DISTINCT partner, county FROM analytics.mart_screener_data WHERE 1=1 [[AND {{submission_date}}]] [[AND {{partner}}]] [[AND {{county}}]]
+    SELECT DISTINCT
+        white_label_id,
+        partner,
+        county
+    FROM analytics.mart_screener_data
+    WHERE 1 = 1 [[AND {{submission_date}}]] [[AND {{partner}}]] [[AND {{county}}]]
 )
+
 SELECT
-    qb.benefit as "Benefit Name",
-    SUM(qb.count) as "# of Screeners",
-    SUM(qb.count)::float / NULLIF(MAX(t.total_count), 0) as "% of Screeners"
-FROM analytics.mart_qualified_benefits qb
-INNER JOIN filter_keys fk ON qb.partner IS NOT DISTINCT FROM fk.partner AND qb.county IS NOT DISTINCT FROM fk.county
-CROSS JOIN totals t
+    qb.benefit AS "Benefit Name",
+    sum(qb.count) AS "# of Screeners",
+    sum(qb.count)::FLOAT / nullif(max(t.total_count), 0) AS "% of Screeners"
+FROM analytics.mart_qualified_benefits AS qb
+INNER JOIN filter_keys AS fk
+    ON
+        qb.partner IS NOT DISTINCT FROM fk.partner
+        AND qb.county IS NOT DISTINCT FROM fk.county
+        AND qb.white_label_id = fk.white_label_id
+CROSS JOIN totals AS t
 GROUP BY qb.benefit
-ORDER BY SUM(qb.count) DESC, qb.benefit ASC
+HAVING sum(qb.count) > 0
+ORDER BY sum(qb.count) DESC, qb.benefit ASC
