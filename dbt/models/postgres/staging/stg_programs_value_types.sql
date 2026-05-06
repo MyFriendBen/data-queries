69b96f3b4afff1ec29b7c49a3ea2eeb4586876cb
IrynaKolh <iryna.kolhanova@gmail.com>
2026-05-06 11:33:22 -0400

    fix data from translation labels

--- dbt/models/postgres/staging/stg_programs_value_types.sql
+++ dbt/models/postgres/staging/stg_programs_value_types.sql
@@ -36,4 +36,4 @@ SELECT
 FROM translations_t AS tt
 INNER JOIN translations_tt AS ttt ON tt.translation_id = ttt.master_id
 INNER JOIN {{ source('django_apps', 'programs_program') }} AS pp
-    ON tt.label LIKE 'program.' || pp.name_abbreviated || '-%'
+    ON tt.label LIKE 'program.' || pp.name_abbreviated || '\_%-%' ESCAPE '\'
